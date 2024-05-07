f1a35b82b6086c30ec4ab3222a4d46da29f0d138
camilasdsantos <99107296+camilasdsantos@users.noreply.github.com>
2024-05-06 23:57:56 -0300

    Create Index.Js

---
+++ Index.Js
@@ -0,0 +1,26 @@
+//declaração de variaveis 
+let nomeHeroi = "João" 
+let xpHeroi = 8001
+let nivel = ""
+
+//Estrutura de decisões(if else) com uso de operadores
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
